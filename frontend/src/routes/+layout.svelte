e8e664fe9966817e2bb14d3c42c5675e38262925
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-03 00:06:30 +0100

    feat: start moving stuff to api

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
+    import '../app.css';
+
     import { ModeWatcher } from 'mode-watcher';
     import { Toaster } from '$lib/components/ui/sonner/index.js';
-    import '../app.css';
-    let { children } = $props();
 
-    import Github from 'lucide-svelte/icons/github';
-    import Gitlab from 'lucide-svelte/icons/gitlab';
-    import HardDrive from 'lucide-svelte/icons/hard-drive';
-    import FileQuestion from 'lucide-svelte/icons/file-question';
+    let { children } = $props();
 
     import AppSidebar from '$lib/components/app-sidebar.svelte';
     import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
@@ -16,83 +13,31 @@
 
     import BrainCircuit from 'lucide-svelte/icons/brain-circuit';
 
-    let user = $state({
-        username: 'norpie',
-        email: 'contact@norpie.dev',
-        avatar: 'https://avatars.githubusercontent.com/u/46564751?v=4'
+    import Spinner from '$lib/components/spinner.svelte';
+
+    import type { User } from '$lib/models/user';
+    import type { Project } from '$lib/models/project';
+    import type { Chat } from '$lib/models/chat';
+    import { onMount } from 'svelte';
+    import { fetchUser } from '$lib/api/user';
+    import { fetchAllProjects } from '$lib/api/project';
+    import { fetchAllChats } from '$lib/api/chat';
+
+    let user: User | null = $state(null);
+    let projects: Project[] | null = $state(null);
+    let selectedProject: Project | null = $state(null);
+    let chats: Chat[] | null = $state(null);
+    let selectedChat: Chat | null = $state(null);
+
+    onMount(async () => {
+        user = await fetchUser();
+        projects = (await fetchAllProjects({ page: 1, pageSize: 10 })).data;
+        selectedProject = projects[0];
+        chats = (await fetchAllChats(selectedProject.id, { page: 1, pageSize: 10 })).data;
+        selectedChat = chats[0];
     });
 
-    function platformLogo(name: string): any {
-        console.log(name);
-        switch (name) {
-            case 'GitHub':
-                return Github;
-            case 'GitLab':
-                return Gitlab;
-            case 'Local':
-                return HardDrive;
-            default:
-                return FileQuestion;
-        }
-    }
-
-    let repositories = $state([
-        {
-            name: 'Mirabel',
-            url: 'https://github.com/norpie/mirabel',
-            platform: 'GitHub',
-            logo: Github
-        },
-        {
-            name: 'Finanalize',
-            url: 'https://github.com/norpie/finanalize',
-            platform: 'GitHub',
-            logo: Github
-        },
-        {
-            name: 'Analytical',
-            url: 'https://github.com/norpie/analytical',
-            platform: 'GitHub',
-            logo: Github
-        },
-        {
-            name: 'Alice',
-            url: 'https://github.com/norpie/alice',
-            platform: 'GitHub',
-            logo: Github
-        },
-        {
-            name: 'µLLM-API',
-            url: 'https://github.com/norpie/uLLM-API',
-            platform: 'GitHub',
-            logo: Github
-        },
-        {
-            name: 'Random Local Project',
-            platform: 'Local',
-            logo: HardDrive
-        }
-    ]);
-
-    // Initialize to the first repository
-    let activeRepository: {
-        name: string;
-        url?: string;
-        platform: string;
-        logo: any;
-    } | null = $state(repositories[0]);
-
-    let chats = $state([
-        {
-            id: 'sdafopihv',
-            title: 'Setting up the repository'
-        }
-    ]);
-
-    let activeChat: {
-        id: string;
-        title: string;
-    } | null = $state(null);
+    $inspect({ user, projects, selectedProject, chats, selectedChat });
 
     let items = $state([
         {
@@ -129,32 +74,38 @@
 <ModeWatcher />
 <Toaster />
 
-<Sidebar.Provider>
-    <AppSidebar bind:user bind:repositories bind:activeRepository bind:chats bind:items />
-    <Sidebar.Inset>
-        <header
-            class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
-        >
-            <div class="flex items-center gap-2 px-4">
-                <Sidebar.Trigger class="-ml-1" />
-                <Separator orientation="vertical" class="mr-2 h-4" />
-                {#if activeRepository}
-                    <Breadcrumb.Root>
-                        <Breadcrumb.List>
-                            <Breadcrumb.Item class="hidden md:block">
-                                <Breadcrumb.Link>{activeRepository.name}</Breadcrumb.Link>
-                            </Breadcrumb.Item>
-                            {#if activeChat}
-                                <Breadcrumb.Separator class="hidden md:block" />
-                                <Breadcrumb.Item>
-                                    <Breadcrumb.Link>{activeChat.title}</Breadcrumb.Link>
+{#if user && projects && selectedProject && chats && selectedChat && items}
+    <Sidebar.Provider>
+        <AppSidebar bind:user bind:projects bind:selectedProject bind:chats bind:items />
+        <Sidebar.Inset>
+            <header
+                class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
+            >
+                <div class="flex items-center gap-2 px-4">
+                    <Sidebar.Trigger class="-ml-1" />
+                    <Separator orientation="vertical" class="mr-2 h-4" />
+                    {#if selectedProject}
+                        <Breadcrumb.Root>
+                            <Breadcrumb.List>
+                                <Breadcrumb.Item class="hidden md:block">
+                                    <Breadcrumb.Link>{selectedProject.name}</Breadcrumb.Link>
                                 </Breadcrumb.Item>
-                            {/if}
-                        </Breadcrumb.List>
-                    </Breadcrumb.Root>
-                {/if}
-            </div>
-        </header>
-        {@render children()}
-    </Sidebar.Inset>
-</Sidebar.Provider>
+                                {#if selectedChat}
+                                    <Breadcrumb.Separator class="hidden md:block" />
+                                    <Breadcrumb.Item>
+                                        <Breadcrumb.Link>{selectedChat.title}</Breadcrumb.Link>
+                                    </Breadcrumb.Item>
+                                {/if}
+                            </Breadcrumb.List>
+                        </Breadcrumb.Root>
+                    {/if}
+                </div>
+            </header>
+            {@render children()}
+        </Sidebar.Inset>
+    </Sidebar.Provider>
+{:else}
+    <div class="flex h-screen w-full items-center justify-center">
+        <Spinner />
+    </div>
+{/if}
