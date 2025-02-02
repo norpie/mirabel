f441a6d53e31e4a40bc9c84263596975d1bfc878
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-03 00:30:13 +0100

    feat: added ready to store

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,4 +1,4 @@
-import { writable, type Writable } from 'svelte/store';
+import { derived, writable, type Writable } from 'svelte/store';
 
 import type { User } from '$lib/models/user';
 import type { Chat } from './models/chat';
@@ -9,3 +9,7 @@ export const projects: Writable<Project[] | null> = writable(null);
 export const selectedProject: Writable<Project | null> = writable(null);
 export const chats: Writable<Chat[] | null> = writable(null);
 export const selectedChat: Writable<Chat | null> = writable(null);
+
+export const ready = derived([user, projects, selectedProject, chats, selectedChat], ([$user, $projects, $selectedProject, $chats, $selectedChat]) => {
+    return !!$user && !!$projects && !!$selectedProject && !!$chats && !!$selectedChat;
+    });
