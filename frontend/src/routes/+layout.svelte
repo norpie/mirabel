812f1ac93774023450b03b6b78e97b850527ee85
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-02 15:35:49 +0100

    refactor: move sidebar into layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,8 +3,145 @@
     import { Toaster } from '$lib/components/ui/sonner/index.js';
     import '../app.css';
     let { children } = $props();
+
+    import Github from 'lucide-svelte/icons/github';
+    import Gitlab from 'lucide-svelte/icons/gitlab';
+    import HardDrive from 'lucide-svelte/icons/hard-drive';
+    import FileQuestion from 'lucide-svelte/icons/file-question';
+
+    import AppSidebar from '$lib/components/app-sidebar.svelte';
+    import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+    import { Separator } from '$lib/components/ui/separator/index.js';
+    import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+
+    import BrainCircuit from 'lucide-svelte/icons/brain-circuit';
+
+    let user = $state({
+        username: 'norpie',
+        email: 'contact@norpie.dev',
+        avatar: 'https://avatars.githubusercontent.com/u/46564751?v=4'
+    });
+
+    function platformLogo(name: string): any {
+        console.log(name);
+        switch (name) {
+            case 'GitHub':
+                return Github;
+            case 'GitLab':
+                return Gitlab;
+            case 'Local':
+                return HardDrive;
+            default:
+                return FileQuestion;
+        }
+    }
+
+    let repositories = $state([
+        {
+            name: 'Mirabel',
+            url: 'https://github.com/norpie/mirabel',
+            platform: 'GitHub',
+            logo: Github
+        },
+        {
+            name: 'Finanalize',
+            url: 'https://github.com/norpie/finanalize',
+            platform: 'GitHub',
+            logo: Github
+        },
+        {
+            name: 'Analytical',
+            url: 'https://github.com/norpie/analytical',
+            platform: 'GitHub',
+            logo: Github
+        },
+        {
+            name: 'Alice',
+            url: 'https://github.com/norpie/alice',
+            platform: 'GitHub',
+            logo: Github
+        },
+        {
+            name: 'µLLM-API',
+            url: 'https://github.com/norpie/uLLM-API',
+            platform: 'GitHub',
+            logo: Github
+        },
+        {
+            name: 'Random Local Project',
+            platform: 'Local',
+            logo: HardDrive
+        }
+    ]);
+
+    // Initialize to the first repository
+    let activeRepository = $state(repositories[0]);
+
+    let chats = $state([
+        {
+            id: 'sdafopihv',
+            title: 'Setting up the repository'
+        }
+    ]);
+
+    let activeChat = $state(null);
+
+    let items = $state([
+        {
+            title: 'Knowledge',
+            icon: BrainCircuit,
+            isActive: false,
+            items: [
+                {
+                    id: 'technologies',
+                    title: 'Technologies'
+                },
+                {
+                    id: 'documentation',
+                    title: 'Documentation'
+                },
+                {
+                    id: 'structure',
+                    title: 'Structure'
+                },
+                {
+                    id: 'workflow',
+                    title: 'Git Workflow'
+                }
+            ]
+        }
+    ]);
 </script>
 
 <ModeWatcher />
 <Toaster />
-{@render children()}
+
+<Sidebar.Provider>
+    <AppSidebar bind:user bind:repositories bind:activeRepository bind:chats bind:items />
+    <Sidebar.Inset>
+        <header
+            class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
+        >
+            <div class="flex items-center gap-2 px-4">
+                <Sidebar.Trigger class="-ml-1" />
+                <Separator orientation="vertical" class="mr-2 h-4" />
+                {#if activeRepository}
+                    <Breadcrumb.Root>
+                        <Breadcrumb.List>
+                            <Breadcrumb.Item class="hidden md:block">
+                                <Breadcrumb.Link>{activeRepository.name}</Breadcrumb.Link>
+                            </Breadcrumb.Item>
+                            {#if activeChat}
+                                <Breadcrumb.Separator class="hidden md:block" />
+                                <Breadcrumb.Item>
+                                    <Breadcrumb.Link>{activeChat.title}</Breadcrumb.Link>
+                                </Breadcrumb.Item>
+                            {/if}
+                        </Breadcrumb.List>
+                    </Breadcrumb.Root>
+                {/if}
+            </div>
+        </header>
+        {@render children()}
+    </Sidebar.Inset>
+</Sidebar.Provider>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,85 +1,11 @@
 <script lang="ts">
-    import AppSidebar from '$lib/components/app-sidebar.svelte';
-    import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
-    import { Separator } from '$lib/components/ui/separator/index.js';
-    import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-
-    import Github from 'lucide-svelte/icons/github';
-
-    import { toast } from 'svelte-sonner';
-
-    let user = $state({
-        username: 'norpie',
-        email: 'contact@norpie.dev',
-        avatar: 'https://avatars.githubusercontent.com/u/46564751?v=4'
-    });
-
-    let repositories = $state([
-        {
-            name: 'Mirabel',
-            url: 'https://github.com/norpie/mirabel',
-            logo: Github,
-            platform: 'GitHub'
-        },
-        {
-            name: 'Finanalize',
-            url: 'https://github.com/norpie/finanalize',
-            logo: Github,
-            platform: 'GitHub'
-        },
-        {
-            name: 'Analytical',
-            url: 'https://github.com/norpie/analytical',
-            logo: Github,
-            platform: 'GitHub'
-        },
-        {
-            name: 'Alice',
-            url: 'https://github.com/norpie/alice',
-            logo: Github,
-            platform: 'GitHub'
-        },
-        {
-            name: 'µLLM-API',
-            url: 'https://github.com/norpie/uLLM-API',
-            logo: Github,
-            platform: 'GitHub'
-        }
-    ]);
-    let activeRepository = $state(repositories[0]);
-
-    let chats = $state([
-        {
-            id: 'sdafopihv',
-            title: 'Setting up the repository'
-        }
-    ]);
-    let activeChat = $state(chats[0]);
 </script>
 
-<Sidebar.Provider>
-    <AppSidebar bind:user bind:repositories bind:activeRepository bind:chats />
-    <Sidebar.Inset>
-        <header
-            class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
-        >
-            <div class="flex items-center gap-2 px-4">
-                <Sidebar.Trigger class="-ml-1" />
-                <Separator orientation="vertical" class="mr-2 h-4" />
-                <Breadcrumb.Root>
-                    <Breadcrumb.List>
-                        <Breadcrumb.Item class="hidden md:block">
-                            <Breadcrumb.Link>{activeRepository.name}</Breadcrumb.Link>
-                        </Breadcrumb.Item>
-                        {#if activeChat}
-                            <Breadcrumb.Separator class="hidden md:block" />
-                            <Breadcrumb.Item>
-                                <Breadcrumb.Link>{activeChat.title}</Breadcrumb.Link>
-                            </Breadcrumb.Item>
-                        {/if}
-                    </Breadcrumb.List>
-                </Breadcrumb.Root>
-            </div>
-        </header>
-    </Sidebar.Inset>
-</Sidebar.Provider>
+<div class="flex flex-1 flex-col gap-4 p-4">
+    <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
+    </div>
+    <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"></div>
+</div>
