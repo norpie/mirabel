0cbc0940460c7c2f7ca6ea83cceae7ed402cdf8c
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-03 00:17:40 +0100

    feat: added store

---
+++ frontend/src/lib/store.ts
@@ -0,0 +1,11 @@
+import { writable, type Writable } from 'svelte/store';
+
+import type { User } from '$lib/models/user';
+import type { Chat } from './models/chat';
+import type { Project } from './models/project';
+
+export const user: Writable<User | null> = writable(null);
+export const projects: Writable<Project[] | null> = writable(null);
+export const selectedProject: Writable<Project | null> = writable(null);
+export const chats: Writable<Chat[] | null> = writable(null);
+export const selectedChat: Writable<Chat | null> = writable(null);
