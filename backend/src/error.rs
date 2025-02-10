75d36ae0b368e9e20d17a814e4b2b426e7c7ea0d
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-10 21:03:41 +0100

    feat: added some more error variants

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -6,8 +6,12 @@ pub enum Error {
     #[error("An error occurred, which uses the generic error type {0}")]
     Generic(String),
 
+    #[error("An error occurred: {0}")]
+    Chrono(#[from] chrono::ParseError),
     #[error("A serde_json error occurred: {0}")]
     SerdeJson(#[from] serde_json::Error),
+    #[error("A reqwest error occurred: {0}")]
+    Reqwest(#[from] reqwest::Error),
 
     #[error("An IO error occurred: {0}")]
     IO(#[from] std::io::Error),
