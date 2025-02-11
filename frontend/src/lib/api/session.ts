173bbc27cf3a92777d3b81994240f8d62dc4a497
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-11 14:12:23 +0100

    feat: added chat to session model

--- frontend/src/lib/api/session.ts
+++ frontend/src/lib/api/session.ts
@@ -1,7 +1,7 @@
-import type { Session } from "$lib/models/session";
+import type { Session, ShallowSession } from "$lib/models/session";
 import type { PageResponse } from "$lib/models/page";
 
-const sampleResponse: PageResponse<Session[]> ={
+const sampleResponse: PageResponse<Session[]> = {
     pageInfo: {
         page: 1,
         pageSize: 10,
@@ -9,16 +9,42 @@ const sampleResponse: PageResponse<Session[]> ={
     data: [
         {
             id: 'sdafopihv',
-            title: 'Setting up the workspace'
+            title: 'Setting up the workspace',
+            chat: {
+                messages: [
+                    {
+                        timestamp: '2021-09-01T12:00:00Z',
+                        author: 'norpie',
+                        message: "Let\'s fix issue #5",
+                    },
+                    {
+                        timestamp: '2021-09-01T12:01:00Z',
+                        author: 'Mirabel',
+                        message: "I'm on it.",
+                    },
+                    {
+                        timestamp: '2021-09-01T12:02:00Z',
+                        author: 'Mirabel',
+                        message: "I've generated a plan. Do you want to approve it?",
+                    },
+                    {
+                        timestamp: '2021-09-01T12:03:00Z',
+                        author: 'norpie',
+                        message: "Yes, please.",
+                    }
+                ],
+
+            }
         }
-    ]};
+    ]
+};
 
 export async function fetchSession(sessionId: string): Promise<Session> {
-    setTimeout(() => {}, 1000);
+    setTimeout(() => { }, 1000);
     return sampleResponse.data[0];
 }
 
-export async function fetchAllSessions(workspaceId: string, page: Page): Promise<PageResponse<Session[]>> {
-    setTimeout(() => {}, 1000);
+export async function fetchAllSessions(workspaceId: string, page: Page): Promise<PageResponse<ShallowSession[]>> {
+    setTimeout(() => { }, 1000);
     return sampleResponse;
 }
--- frontend/src/lib/models/session.ts
+++ frontend/src/lib/models/session.ts
@@ -1,4 +1,20 @@
+export interface ShallowSession {
+    id: string;
+    title: string;
+}
+
 export interface Session {
     id: string;
     title: string;
+    chat: Chat;
+}
+
+export interface Chat {
+    messages: Message[];
+}
+
+export interface Message {
+    timestamp: string;
+    author: string;
+    message: string;
 }
