44c6cb1e44b57bae31f3f8564e40050f41f9db4b
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-11 10:06:49 +0100

    feat: use generateId for projects

--- frontend/src/lib/api/project.ts
+++ frontend/src/lib/api/project.ts
@@ -1,6 +1,8 @@
 import type { Page, PageResponse } from '$lib/models/page';
 import type { Project } from '$lib/models/project';
 
+import { generateId } from '$lib/utils';
+
 import Github from 'lucide-svelte/icons/github';
 import Gitlab from 'lucide-svelte/icons/gitlab';
 import HardDrive from 'lucide-svelte/icons/hard-drive';
@@ -13,36 +15,42 @@ const sampleResponse: PageResponse<Project[]> = {
     },
     data: [
         {
+            id: generateId(),
             name: 'Mirabel',
             url: 'https://github.com/norpie/mirabel',
             platform: 'GitHub',
             icon: Github
         },
         {
+            id: generateId(),
             name: 'Finanalize',
             url: 'https://github.com/norpie/finanalize',
             platform: 'GitHub',
             icon: Github
         },
         {
+            id: generateId(),
             name: 'Analytical',
             url: 'https://github.com/norpie/analytical',
             platform: 'GitHub',
             icon: Github
         },
         {
+            id: generateId(),
             name: 'Alice',
             url: 'https://github.com/norpie/alice',
             platform: 'Gitlab',
             icon: Gitlab
         },
         {
+            id: generateId(),
             name: 'µLLM-API',
             url: 'https://github.com/norpie/uLLM-API',
             platform: 'Bitbucket',
             icon: FileQuestion
         },
         {
+            id: generateId(),
             name: 'Random Local Project',
             platform: 'Local',
             icon: HardDrive
@@ -51,6 +59,11 @@ const sampleResponse: PageResponse<Project[]> = {
 };
 
 export async function fetchAllProjects(page: Page): Promise<PageResponse<Project[]>> {
-    setTimeout(() => {}, 1000);
+    setTimeout(() => { }, 1000);
     return sampleResponse;
+}
+
+export async function fetchRecentProject(): Promise<Project> {
+    setTimeout(() => { }, 1000);
+    return sampleResponse.data[0];
 }
