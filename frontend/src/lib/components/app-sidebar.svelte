6665d0c727b3f64326ff3fc66071f8e8e479531f
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-11 11:37:17 +0100

    refactor: rename nav items and switcher

--- frontend/src/lib/components/app-sidebar.svelte
+++ frontend/src/lib/components/app-sidebar.svelte
@@ -1,43 +1,43 @@
 <script lang="ts">
-    import NavMain from '$lib/components/nav-main.svelte';
-    import NavWorkspaces from '$lib/components/nav-workspaces.svelte';
-    import NavUser from '$lib/components/nav-user.svelte';
-    import TeamSwitcher from '$lib/components/team-switcher.svelte';
-    import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import NavSettings from '$lib/components/nav-settings.svelte';
+	import NavChats from '$lib/components/nav-chats.svelte';
+	import NavUser from '$lib/components/nav-user.svelte';
+	import WorkspaceSwitcher from '$lib/components/workspace-switcher.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 
-    let {
-        items = $bindable(),
+	let {
+		items = $bindable(),
 
-        ref = $bindable(null),
-        collapsible = 'icon',
-        ...restProps
-    }: {
-        items: {
-            title: string;
-            url: string;
-            icon: any;
-            isActive: boolean;
-            items: {
-                title: string;
-                url: string;
-            }[];
-        }[];
+		ref = $bindable(null),
+		collapsible = 'icon',
+		...restProps
+	}: {
+		items: {
+			title: string;
+			url: string;
+			icon: any;
+			isActive: boolean;
+			items: {
+				title: string;
+				url: string;
+			}[];
+		}[];
 
-        ref?: any;
-        collapsible?: 'offcanvas' | 'icon' | 'none';
-    } = $props();
+		ref?: any;
+		collapsible?: 'offcanvas' | 'icon' | 'none';
+	} = $props();
 </script>
 
 <Sidebar.Root bind:ref {collapsible} {...restProps}>
-    <Sidebar.Header>
-        <TeamSwitcher />
-    </Sidebar.Header>
-    <Sidebar.Content>
-        <NavMain {items} />
-        <NavWorkspaces />
-    </Sidebar.Content>
-    <Sidebar.Footer>
-        <NavUser />
-    </Sidebar.Footer>
-    <Sidebar.Rail />
+	<Sidebar.Header>
+		<WorkspaceSwitcher />
+	</Sidebar.Header>
+	<Sidebar.Content>
+		<NavSettings {items} />
+		<NavChats />
+	</Sidebar.Content>
+	<Sidebar.Footer>
+		<NavUser />
+	</Sidebar.Footer>
+	<Sidebar.Rail />
 </Sidebar.Root>
