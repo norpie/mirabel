bf7fa397a4f6a1c9736602864ee26ffbb7eecadc
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-11 10:06:32 +0100

    feat: added generateId()

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -2,5 +2,16 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
+}
+
+const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+const charsLength = chars.length;
+
+export function generateId(): string {
+    let result = "";
+    for (let counter = 0; counter < 20; counter++) {
+        result += chars.charAt(Math.floor(Math.random() * charsLength));
+    }
+    return result;
 }
