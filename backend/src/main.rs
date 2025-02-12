0e92971cc8dd1a857e482936ac57c48eb4992f0f
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-12 15:38:17 +0100

    feat: added backend structure

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,6 +4,10 @@ pub(crate) mod error;
 pub(crate) mod prelude;
 
 pub(crate) mod driver;
+pub(crate) mod handler;
+pub(crate) mod model;
+pub(crate) mod repository;
+pub(crate) mod service;
 
 fn main() {
     println!("Hello, world!");
