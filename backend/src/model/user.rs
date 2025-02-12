e0a3c4f4013861a52788a339cbff388a77222d46
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-12 15:46:21 +0100

    feat: added user model

---
+++ backend/src/model/user.rs
@@ -0,0 +1,24 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct User {
+    id: String,
+    username: String,
+    email: String,
+    #[serde(skip_serializing)]
+    password: String,
+    created_at: DateTime<Utc>,
+}
+
+impl User {
+    pub fn new(id: String, username: String, email: String, password: String) -> Self {
+        User {
+            id,
+            username,
+            email,
+            password,
+            created_at: Utc::now(),
+        }
+    }
+}
