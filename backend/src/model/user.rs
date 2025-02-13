5d396cd380370af492e1f36c7956d130c85bb36c
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-13 19:33:15 +0100

    feat: added more user types

--- backend/src/model/user.rs
+++ backend/src/model/user.rs
@@ -2,12 +2,42 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct NewUser {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct UpdatedUser {
+    pub username: Option<String>,
+    pub email: Option<String>,
+    pub password: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct User {
     id: String,
     username: String,
     email: String,
-    #[serde(skip_serializing)]
     password: String,
+    created_at: DateTime<Utc>,
+}
+
+impl From<User> for FrontendUser {
+    fn from(user: User) -> Self {
+        FrontendUser {
+            username: user.username,
+            email: user.email,
+            created_at: user.created_at,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FrontendUser {
+    username: String,
+    email: String,
     #[serde(rename = "createdAt")]
     created_at: DateTime<Utc>,
 }
