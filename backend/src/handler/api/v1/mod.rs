8e870abe406b38d636f846030e292fc9de1926dd
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-13 18:05:28 +0100

    feat: hello world can error for testing purposes

--- backend/src/handler/api/v1/mod.rs
+++ backend/src/handler/api/v1/mod.rs
@@ -1,5 +1,16 @@
-use actix_web::{web, Scope};
+use crate::{dto::api_response::ApiResponse, prelude::*};
+
+use actix_web::{get, web, Responder, Scope};
 
 pub fn scope() -> Scope {
-    web::scope("/v1").route("/hello", web::get().to(|| async { "hello world" }))
+    web::scope("/v1").service(hello)
+}
+
+#[get("/hello")]
+pub async fn hello() -> Result<impl Responder> {
+    let random: bool = rand::random();
+    if random {
+        return Err("Random error".into());
+    }
+    Ok(ApiResponse::ok("Hello, World!"))
 }
