747e3fc053dc881c22fe1a474f1d75c792a1e1b6
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-13 18:10:15 +0100

    feat: added some http error types

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -3,9 +3,25 @@ use thiserror::Error;
 
 #[derive(Debug, Error, Diagnostic)]
 pub enum Error {
+    // Generic error type
     #[error("Generic error, with message: {0}")]
     Generic(String),
 
+    // 400.. HTTP error types
+    #[error("Bad request: {0}")]
+    BadRequest(String),
+    #[error("Unauthorized: {0}")]
+    Unauthorized(String),
+    #[error("Forbidden: {0}")]
+    Forbidden(String),
+    #[error("Resource not found: {0}")]
+    NotFound(String),
+
+    // 500.. HTTP error types
+    #[error("Internal server error")]
+    InternalServer,
+
+    // Library error types
     #[error("An actix error occurred: {0}")]
     ActixWeb(#[from] actix_web::Error),
     #[error("A chrono error occurred: {0}")]
@@ -17,6 +33,7 @@ pub enum Error {
     #[error("A reqwest_streams error occurred: {0}")]
     StreamBody(#[from] reqwest_streams::error::StreamBodyError),
 
+    // `std`-error types
     #[error("An IO error occurred: {0}")]
     IO(#[from] std::io::Error),
     #[error("An error occurred while parsing an integer: {0}")]
