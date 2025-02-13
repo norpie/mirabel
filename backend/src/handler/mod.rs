bd34c2931fb6e739336a76d0923b5f730bb5c672
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-13 13:30:52 +0100

    feat: added print to handler

--- backend/src/handler/mod.rs
+++ backend/src/handler/mod.rs
@@ -8,6 +8,7 @@ use actix_web::{web, App, HttpResponse, HttpServer};
 pub async fn run() -> Result<()> {
     let host = env::var("BACKEND_HOST")?;
     let port: u16 = env::var("BACKEND_PORT")?.parse()?;
+    println!("Listening on {}:{}", host, port);
     HttpServer::new(move || {
         let cors = Cors::default()
             .allowed_origin_fn(|_, _| true)
