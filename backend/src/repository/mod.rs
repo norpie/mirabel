cbc51d687c97b340c5b2eb85edf1faab487f2938
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-14 16:45:40 +0100

    feat: added trait work workspace repository

--- backend/src/repository/mod.rs
+++ backend/src/repository/mod.rs
@@ -6,7 +6,9 @@ use async_trait::async_trait;
 use users::UserRepository;
 
 pub(crate) mod surrealdb;
+
 pub(crate) mod users;
+pub(crate) mod workspaces;
 
 #[async_trait]
 pub trait Repository: Send + Sync + UserRepository {}
---
+++ backend/src/repository/workspaces.rs
@@ -0,0 +1,30 @@
+use crate::{
+    dto::page::PageRequest,
+    model::{
+        user::{NewUser, UpdatedUser, User},
+        workspace::{NewWorkspace, UpdatedWorkspace, Workspace},
+    },
+    prelude::*,
+};
+
+use async_trait::async_trait;
+
+use super::Repository;
+
+#[async_trait]
+pub trait WorkspaceRepository {
+    /// Create a new workspace.
+    async fn create_workspace(&self, workspace: NewWorkspace) -> Result<Workspace>;
+
+    /// Retrieve a workspace by ID.
+    async fn get_workspace_by_id(&self, id: String) -> Result<Option<Workspace>>;
+
+    /// Retrieve user's workspaces.
+    async fn get_users_workspaces(&self, page: PageRequest) -> Result<Vec<Workspace>>;
+
+    /// Update an existing workspace.
+    async fn update_workspace(&self, id: String, workspace: UpdatedWorkspace) -> Result<Workspace>;
+
+    /// Delete a workspace.
+    async fn delete_workspace(&self, id: String) -> Result<Workspace>;
+}
