e168f3eb629bc352bb89796beffb8e47d2c2c432
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-14 13:57:22 +0100

    feat: remove hello world endpoint

--- backend/src/handler/api/v1/mod.rs
+++ backend/src/handler/api/v1/mod.rs
@@ -7,17 +7,7 @@ pub mod me;
 pub mod users;
 
 pub fn scope(cfg: &mut web::ServiceConfig) {
-    cfg.service(hello);
     cfg.service(Scope::new("/v1").configure(auth::scope));
     cfg.service(Scope::new("/v1").configure(me::scope));
     cfg.service(Scope::new("/v1").configure(users::scope));
-}
-
-#[get("/hello")]
-pub async fn hello() -> Result<impl Responder> {
-    let random: bool = rand::random();
-    if random {
-        return Err("Random error".into());
-    }
-    Ok(ApiResponse::ok("Hello, World!"))
 }
