f4236b9aca267bf26a23d5bc383b74b9ff939fa7
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-14 10:48:30 +0100

    feat: added user extractor from request

---
+++ backend/src/handler/extractors.rs
@@ -0,0 +1,29 @@
+use crate::prelude::*;
+
+use std::{future::Future, pin::Pin};
+
+use actix_web::{web::Data, FromRequest, HttpMessage, HttpRequest};
+
+use crate::{model::user::User, repository::Repository};
+
+use super::Error;
+
+impl FromRequest for User {
+    type Error = Error;
+
+    type Future = Pin<Box<dyn Future<Output = Result<Self>>>>;
+
+    fn from_request(req: &HttpRequest, _payload: &mut actix_web::dev::Payload) -> Self::Future {
+        let id_opt = req.extensions().get::<String>().cloned();
+        let db_opt = req.app_data::<Data<Box<dyn Repository>>>().cloned();
+
+        Box::pin(async move {
+            let id = id_opt.ok_or(Error::Unauthorized("Invalid Token".into()))?;
+            let db = db_opt.ok_or(Error::InternalServer)?;
+
+            db.get_user_by_id(id)
+                .await?
+                .ok_or(Error::Unauthorized("Invalid Token".into()))
+        })
+    }
+}
