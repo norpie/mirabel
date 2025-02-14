56b0a845995c8f655b2ae59df8d737eeb95b7194
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-14 14:18:36 +0100

    feat: added me and update_me

--- backend/src/handler/api/v1/me.rs
+++ backend/src/handler/api/v1/me.rs
@@ -1,5 +1,37 @@
-use actix_web::{web, Scope};
+use crate::{model::user::UpdatedUser, prelude::*, repository::Repository, service::users};
+
+use actix_web::{
+    get, patch,
+    web::{self, Data, Json},
+    Responder, Scope,
+};
+
+use crate::{
+    dto::api_response::ApiResponse,
+    handler::middleware::auth_middleware::Auth,
+    model::user::{FrontendUser, User},
+};
 
 pub fn scope(cfg: &mut web::ServiceConfig) {
-    cfg.service(Scope::new("/me"));
+    cfg.service(
+        Scope::new("/me")
+            .wrap(Auth)
+            .service(get_me)
+            .service(update_me),
+    );
+}
+
+#[get("")]
+pub async fn get_me(user: User) -> Result<impl Responder> {
+    Ok(ApiResponse::ok(FrontendUser::from(user)))
+}
+
+#[patch("")]
+pub async fn update_me(
+    db: Data<Box<dyn Repository>>,
+    user: User,
+    updated_user: Json<UpdatedUser>,
+) -> Result<impl Responder> {
+    let user = users::update(db, user, updated_user.0).await?;
+    Ok(ApiResponse::ok(FrontendUser::from(user)))
 }
