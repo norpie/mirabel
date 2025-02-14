ed3cce196edca17bfb7822cfa8a077e5ef641e6a
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-14 14:18:19 +0100

    feat: added user service for updating user

--- backend/src/service/mod.rs
+++ backend/src/service/mod.rs
@@ -1,2 +1,3 @@
 pub(crate) mod auth;
 pub(crate) mod security;
+pub(crate) mod users;
---
+++ backend/src/service/users.rs
@@ -0,0 +1,28 @@
+use crate::prelude::*;
+
+use actix_web::web::Data;
+use argon2::{
+    password_hash::{rand_core::OsRng, SaltString},
+    Argon2, PasswordHasher,
+};
+
+use crate::{
+    model::user::{AuthUser, UpdatedUser, User},
+    repository::Repository,
+};
+
+pub async fn update(
+    db: Data<Box<dyn Repository>>,
+    user: User,
+    mut updated_user: UpdatedUser,
+) -> Result<User> {
+    if let Some(password) = updated_user.password {
+        let argon2 = Argon2::default();
+        let salt = SaltString::generate(&mut OsRng);
+        let password_hash = argon2
+            .hash_password(password.as_bytes(), &salt)?
+            .to_string();
+        updated_user.password = Some(password_hash);
+    };
+    db.update_user(user.id().to_string(), updated_user).await
+}
