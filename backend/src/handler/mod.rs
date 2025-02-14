36906956bbf7a85554bcf84c8789e99b39c10778
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-02-14 12:36:00 +0100

    feat: added middleware logger

--- backend/src/handler/mod.rs
+++ backend/src/handler/mod.rs
@@ -7,17 +7,20 @@ use std::{env, sync::Arc};
 
 use actix_cors::Cors;
 use actix_web::{
-    web::{self, Data},
-    App, HttpResponse, HttpServer,
+    middleware::Logger, web::{self, Data}, App, HttpResponse, HttpServer, Scope
 };
 
 mod api;
+pub(crate) mod extractors;
+pub(crate) mod middleware;
 
 pub async fn run(db: Data<Box<dyn Repository>>) -> Result<()> {
     let host = env::var("BACKEND_HOST")?;
     let port: u16 = env::var("BACKEND_PORT")?.parse()?;
     println!("Listening on {}:{}", host, port);
     HttpServer::new(move || {
+        let logger = Logger::default();
+
         let cors = Cors::default()
             .allowed_origin_fn(|_, _| true)
             .allow_any_method()
@@ -28,6 +31,7 @@ pub async fn run(db: Data<Box<dyn Repository>>) -> Result<()> {
         App::new()
             .app_data(db.clone())
             .wrap(cors)
+            .wrap(logger)
             .configure(api::scope)
             .default_service(web::route().to(HttpResponse::NotFound))
     })
