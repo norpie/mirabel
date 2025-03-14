28e5a4381c0e66fd797b4c6aa4167a23d601d2d8
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-03-14 23:58:37 +0100

    build: added dialog

--- frontend/src/lib/components/nav-user.svelte
+++ frontend/src/lib/components/nav-user.svelte
@@ -58,8 +58,8 @@
 					<DropdownMenu.Label class="p-0 font-normal">
 						<div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
 							<Avatar.Root class="h-8 w-8 rounded-lg">
-								<Avatar.Image src={$user.avatar} alt={$user.username} />
-								<Avatar.Fallback class="rounded-lg">CN</Avatar.Fallback>
+								<Avatar.Image src={$avatar} alt={$user.username} />
+								<Avatar.Fallback class="rounded-lg">{$user.username[0]}</Avatar.Fallback>
 							</Avatar.Root>
 							<div class="grid flex-1 text-left text-sm leading-tight">
 								<span class="truncate font-semibold">{$user.username}</span>
