bade2b42f394e8394ca93e2976e91b5d5bae4392
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-03-15 23:58:39 +0100

    refactor: default to empty array of workspaces

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -6,7 +6,7 @@ import type { Workspace } from './models/workspace';
 
 export const user: Writable<User | null | undefined> = writable(undefined);
 export const avatar: Writable<string | null | undefined> = writable(null);
-export const workspaces: Writable<Workspace[] | null> = writable(null);
+export const workspaces: Writable<Workspace[]> = writable([]);
 export const selectedWorkspace: Writable<Workspace | null> = writable(null);
 export const sessions: Writable<ShallowSession[] | null> = writable(null);
 export const selectedSession: Writable<Session | null> = writable(null);
