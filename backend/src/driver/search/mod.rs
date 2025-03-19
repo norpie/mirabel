c294cb403dfc8cfe06be6137441cbf59957da115
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-03-19 21:00:28 +0100

    feat: add wrapper struct for search engines, to allow interacting with search without knowledge of underlying

--- backend/src/driver/search/mod.rs
+++ backend/src/driver/search/mod.rs
@@ -1,9 +1,38 @@
+use std::collections::HashMap;
+
 use crate::prelude::*;
 
 use async_trait::async_trait;
+use searxng::SearxNG;
 use serde::{Deserialize, Serialize};
 
 pub mod searxng;
+
+pub struct Engines {
+    engines: Vec<Box<dyn SearchEngine>>,
+}
+
+#[async_trait]
+impl SearchEngine for Engines {
+    async fn search(&self, query: String, page: i32) -> Result<SearchPage> {
+        // Priority is given to the first available engine
+        for engine in &self.engines {
+            if engine.available().await {
+                return engine.search(query, page).await;
+            }
+        }
+        Err(Error::NoAvailableEngine)
+    }
+
+    async fn available(&self) -> bool {
+        for engine in &self.engines {
+            if engine.available().await {
+                return true;
+            }
+        }
+        false
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SearchPage {
