c01633ef8bb8bf2ee3384c6e8dc655d690746dac
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-05-10 17:18:06 +0200

    build: update breadcrumb comp

--- frontend/src/lib/components/ui/breadcrumb/breadcrumb-link.svelte
+++ frontend/src/lib/components/ui/breadcrumb/breadcrumb-link.svelte
@@ -1,29 +1,31 @@
 <script lang="ts">
-    import type { HTMLHtmlAttributes } from 'svelte/elements';
-    import type { Snippet } from 'svelte';
-    import type { WithElementRef } from 'bits-ui';
-    import { cn } from '$lib/utils.js';
+	import type { HTMLAnchorAttributes } from "svelte/elements";
+	import type { Snippet } from "svelte";
+	import type { WithElementRef } from "bits-ui";
+	import { cn } from "$lib/utils.js";
 
-    let {
-        ref = $bindable(null),
-        class: className,
-        child,
-        children,
-        ...restProps
-    }: WithElementRef<HTMLHtmlAttributes> & {
-        child?: Snippet<[{ props: HTMLHtmlAttributes }]>;
-    } = $props();
+	let {
+		ref = $bindable(null),
+		class: className,
+		href = undefined,
+		child,
+		children,
+		...restProps
+	}: WithElementRef<HTMLAnchorAttributes> & {
+		child?: Snippet<[{ props: HTMLAnchorAttributes }]>;
+	} = $props();
 
-    const attrs = $derived({
-        class: cn('text-foreground', className),
-        ...restProps
-    });
+	const attrs = $derived({
+		class: cn("hover:text-foreground transition-colors", className),
+		href,
+		...restProps,
+	});
 </script>
 
 {#if child}
-    {@render child({ props: attrs })}
+	{@render child({ props: attrs })}
 {:else}
-    <p bind:this={ref} {...attrs}>
-        {@render children?.()}
-    </p>
+	<a bind:this={ref} {...attrs}>
+		{@render children?.()}
+	</a>
 {/if}
