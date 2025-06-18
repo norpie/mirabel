31d18c26dcd51b2d35b4ee5b005fc5b27dcc5a92
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-06-19 01:20:11 +0200

    feat: redirect to not-found on not found workspace + sessions

--- frontend/src/routes/workspace/[workspace_id]/+page.ts
+++ frontend/src/routes/workspace/[workspace_id]/+page.ts
@@ -5,15 +5,24 @@ import { getWorkspaceById, sessions } from '$lib/store';
 import { load as loadRoot } from "../../+page";
 
 import { toast } from 'svelte-sonner';
+import { goto } from '$app/navigation';
 
 export async function load({ params }: PageLoad) {
     await loadRoot({ params });
     console.log('load workspace page');
 	const page = { page: 1, size: 10 };
+    const workspace = getWorkspaceById(params.workspace_id);
+    if (!workspace) {
+        goto("/not-found");
+        return;
+    }
 	let sessions = await fetchAllSessions(params.workspace_id, page);
+    if (!sessions) {
+        goto("/not-found");
+    }
     return {
         id: params.workspace_id,
         sessions: sessions.data,
-        workspace: getWorkspaceById(params.workspace_id),
+        workspace: workspace,
     };
 };
--- frontend/src/routes/workspace/[workspace_id]/session/[session_id]/+page.ts
+++ frontend/src/routes/workspace/[workspace_id]/session/[session_id]/+page.ts
@@ -2,10 +2,17 @@ import type { PageLoad } from './$types';
 import { fetchSession } from '$lib/api/session';
 
 import { load as loadWorkspace } from "../../+page";
+import { goto } from '$app/navigation';
 
 export async function load({params}: PageLoad) {
     let workspaceLoad = await loadWorkspace({params});
+    if (!workspaceLoad) {
+        goto("/not-found");
+    }
     const session = await fetchSession(params.session_id)
+    if (!session) {
+        goto("/not-found");
+    }
     return {
         id: params.session_id,
         session,
