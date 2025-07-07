50bf735ec9345d27be482f05710edf3a660b78c6
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-07-08 00:27:51 +0300

    chore: remove old plan and chat models

--- backend/src/dto/session/mod.rs
+++ backend/src/dto/session/mod.rs
@@ -1,8 +1,6 @@
 use serde::{Deserialize, Serialize};
 
 use crate::model::{
-    chat::{Chat, ChatParticipant},
-    plan::Plan,
     session::Session,
 };
 
@@ -12,12 +10,8 @@ pub mod event;
 pub struct FullSession {
     pub id: String,
     pub title: String,
-    pub participants: Vec<ChatParticipant>,
-    pub chat: Option<Chat>,
-    pub plan: Option<Plan>,
     pub terminal: Option<Vec<String>>,
     pub archived: bool,
-    // pub actions: Vec<Action>,
 }
 
 impl From<Session> for FullSession {
@@ -25,9 +19,6 @@ impl From<Session> for FullSession {
         Self {
             id: session.id,
             title: session.title,
-            participants: vec![],
-            chat: None,
-            plan: None,
             terminal: None,
             archived: session.archived,
         }
--- backend/src/model/mod.rs
+++ backend/src/model/mod.rs
@@ -1,5 +1,3 @@
-pub(crate) mod chat;
-pub(crate) mod plan;
 pub(crate) mod session;
 pub(crate) mod user;
 pub(crate) mod workspace;
