548d6f3da81102610799a7caae5f16ee0f7fafed
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-07-09 23:40:02 +0200

    fix: whatever went wrong with the initial migrations

--- backend/migrations/2025-07-06-141151_create_user_basics/down.sql
+++ backend/migrations/2025-07-06-141151_create_user_basics/down.sql
@@ -1,19 +1,5 @@
 -- This file should undo anything in `up.sql`
-CREATE TABLE "_welds_migrations"(
-	"id" INT8 NOT NULL PRIMARY KEY,
-	"name" TEXT NOT NULL,
-	"when_applied" INT8 NOT NULL,
-	"rollback_sql" TEXT NOT NULL
-);
 
-ALTER TABLE "auth_options" DROP COLUMN "two_factor_encoded";
-ALTER TABLE "auth_options" ADD COLUMN "two_factor_encoded" TEXT NOT NULL;
-
-
-CREATE TABLE "deleted_users"(
-	"id" TEXT NOT NULL PRIMARY KEY,
-	"user_id" TEXT NOT NULL,
-	FOREIGN KEY ("user_id") REFERENCES "users"("id")
-);
-
-
+DROP TABLE IF EXISTS auth_options;
+DROP TABLE IF EXISTS avatars;
+DROP TABLE IF EXISTS users;
--- backend/migrations/2025-07-06-141151_create_user_basics/up.sql
+++ backend/migrations/2025-07-06-141151_create_user_basics/up.sql
@@ -1,8 +1,23 @@
 -- Your SQL goes here
-DROP TABLE IF EXISTS "_welds_migrations";
-ALTER TABLE "auth_options" DROP COLUMN "two_factor_encoded";
-ALTER TABLE "auth_options" ADD COLUMN "two_factor_encoded" TEXT;
 
+CREATE TABLE users (
+    id text NOT NULL PRIMARY KEY,
+    username text NOT NULL,
+    email text NOT NULL,
+    password text NOT NULL,
+    created_at timestamp NOT NULL,
+    modified_at timestamp NOT NULL
+);
 
-DROP TABLE IF EXISTS "deleted_users";
+CREATE TABLE avatars (
+    id text NOT NULL PRIMARY KEY,
+    user_id text NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
+CREATE TABLE auth_options (
+    id text NOT NULL PRIMARY KEY,
+    user_id text NOT NULL,
+    two_factor_encoded text,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
