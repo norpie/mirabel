fae432840a8797ff544269fee1d837387897ae34
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-07-23 14:26:39 +0200

    fix: warning about node only on browser build

--- mirabel-web/vite.config.ts
+++ mirabel-web/vite.config.ts
@@ -5,5 +5,12 @@ export default defineConfig({
     plugins: [sveltekit()],
     optimizeDeps: {
         exclude: ['@battlefieldduck/xterm-svelte']
+    },
+    build: {
+        rollupOptions: {
+            external: (id) => {
+                return id.startsWith('node:')
+            }
+        }
     }
 });
